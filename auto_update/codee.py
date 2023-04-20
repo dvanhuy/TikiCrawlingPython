406d1735a0ee37ddd46663c049d2c741cdd6d98f
unknown <huydinhvan13@gmail.com>
2023-04-20 20:49:11 +0700

    gop lai 4 muc

--- auto_update/codee.py
+++ auto_update/codee.py
@@ -1,16 +1,22 @@
 import datetime
 import mysql.connector
 import requests
+import json
 print('%s   -   %s.\n' % (datetime.datetime.now(),"Update" ))
 
-mysql3 = mysql.connector.connect(
+mysqlconect = mysql.connector.connect(
         host="mysql_service",
         user="root",
         password="123",
         port='3306',
         database="pythonquanlylaptop"
     )
-mycursor = mysql3.cursor()
+mycursor = mysqlconect.cursor()
+sql = "SELECT count(id) FROM LapTop"
+mycursor.execute(sql)
+myresult = mycursor.fetchall()
+temp = json.dumps(myresult)
+socot = json.loads(temp)[0][0]
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64; rv:83.0) Gecko/20100101 Firefox/83.0',
     'Accept': 'application/json, text/plain, */*',
@@ -31,20 +37,44 @@ params = {
     'sort': 'newest',
     'urlKey':  'laptop',
 }
-
 sql = "insert into LapTop (id,name_lap,brand_name,price,original_price,discount,discount_rate,rating_average,review_count,thumbnail_url,thumbnail_height,thumbnail_width,video_url,seller_id) values (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
-response = requests.get('https://tiki.vn/api/personalish/v1/blocks/listings', headers=headers, params=params)#, cookies=cookies)
-if response.status_code == 200:
-    for record in response.json().get('data'):
-        temp = (record.get('id'),record.get('name'), record.get('brand_name'),record.get('price'),
-                record.get('original_price'),record.get('discount'),record.get('discount_rate'),record.get('rating_average'),
-                record.get('review_count'),record.get('thumbnail_url'),record.get('thumbnail_height'),
-                record.get('thumbnail_width'), record.get('video_url'), record.get('seller_id'))
-        try:
-            mycursor.execute(sql,temp)
-            mysql3.commit()
-            print(temp)
-        except:
-            break
-mysql3.close()        
-mycursor.close()
+
+if socot>100:
+    response = requests.get('https://tiki.vn/api/personalish/v1/blocks/listings', headers=headers, params=params)#, cookies=cookies)
+    if response.status_code == 200:
+        for record in response.json().get('data'):
+            temp = (record.get('id'),record.get('name'), record.get('brand_name'),record.get('price'),
+                    record.get('original_price'),record.get('discount'),record.get('discount_rate'),record.get('rating_average'),
+                    record.get('review_count'),record.get('thumbnail_url'),record.get('thumbnail_height'),
+                    record.get('thumbnail_width'), record.get('video_url'), record.get('seller_id'))
+            try:
+                mycursor.execute(sql,temp)
+                mysqlconect.commit()
+                print(temp)
+            except:
+                break
+    mysqlconect.close()        
+    mycursor.close()
+else:
+    dataid =[]
+    for i in range(1, 11):
+        params['page'] = i
+        response = requests.get('https://tiki.vn/api/personalish/v1/blocks/listings', headers=headers, params=params)#, cookies=cookies)
+        if response.status_code == 200:
+            print('request success!!!')
+            val = []
+            for record in response.json().get('data'):
+                temp = (record.get('id'),record.get('name'), record.get('brand_name'),record.get('price'),
+                        record.get('original_price'),record.get('discount'),record.get('discount_rate'),record.get('rating_average'),
+                        record.get('review_count'),record.get('thumbnail_url'),record.get('thumbnail_height'),
+                        record.get('thumbnail_width'), record.get('video_url'), record.get('seller_id'))
+                if (record.get('id') not in dataid):
+                    val.append(temp)
+                dataid.append(record.get('id'))
+            
+            mycursor.executemany(sql,val)
+            mysqlconect.commit()
+            print("Crawling")    
+            print(val)
+    mysqlconect.close()
+    mycursor.close()    
